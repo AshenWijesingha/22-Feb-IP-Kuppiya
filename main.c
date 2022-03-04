542bf5f226336bfb0268378324328715b6646b54
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2022-03-04 15:23:10 +0000

    scanf Function

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  int num1, num2, sum;
+
+  printf("Enter Num1 Value = ");
+  scanf("%d",&num1);
+  
+  printf("Enter Num2 Value = ");
+  scanf("%d",&num2);
+  
+
+  sum = num1 + num2;
+  printf("\nSum Value of num1 and num2 is = %d",sum);
+
   return 0;
 }
