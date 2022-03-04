6b97bff3dec876e731f9cf1dca3602c6f87594ff
Ashen_Wijesingha <66056859+AshenWijesingha@users.noreply.github.com>
2022-03-04 15:32:04 +0000

    Scanf-t1

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  int number1;
+  float number2;
+  double number3;
+  char character;
+
+  printf("Input Value For number1 = ");
+  scanf("%d",&number1);
+
+  printf("Input Value For number2 = ");
+  scanf("%f",&number2);
+
+  printf("Input Value For number3 = ");
+  scanf("%f",&number3);
+
+  printf("Input Value For character = ");
+  scanf("% c",&character);
+
+  printf("\v%d - %f - %f - % c ", number1, number2, number3, 'character');
+  
+  
   return 0;
 }
